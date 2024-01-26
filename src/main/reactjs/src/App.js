74621b2413ea0bc9ca2c028f86b095eb118e4f47
김동현 <kd0948@naver.com>
2024-01-26 17:51:11 +0900

    third test

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import mainImg from './image/10.png';
+import pam from './image/simson.jpg';
 
 function App() {
   return (
     <div className="App">
       <img alt='' src={mainImg} />
       <h2><b>스프링부트+리액트 CI/CD 성공기원 !!!! <br /> 한번에 성공하기</b></h2>
+      <img alt='' src={pam} />
+      <h1>축하해 성공이야</h1>
+
     </div>
   );
 }
